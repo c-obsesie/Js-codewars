719a60163cef530e720b78f5ccbc42ca089915f2
c-obsesie <56051151+c-obsesie@users.noreply.github.com>
2020-03-17 13:31:58 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -9,9 +9,20 @@ function firstNonConsecutive(arr) {
   return null
 }
 // No oddities here
+
 function noOdds( values ){
   // Return all non-odd values
   return values.filter(val=>val%2===0);
 }
 
-//Maximum
+//Your task is to write a function which returns the sum of following series upto nth term(parameter).
+//Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+
+function SeriesSum(n) {
+  for (var s = 0, i = 0; i < n; i++) {
+    s += 1 / (1 + i * 3)
+  }
+  
+  return s.toFixed(2)
+}
+
